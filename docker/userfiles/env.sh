a17f71e915ecb080580fade300759c4fbf54b2b2
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-09-16 14:14:22 +0200

    Hosts

--- docker/userfiles/env.sh
+++
@@ -1,1 +0,0 @@
-export ID="$(env | grep SSH_CONNECTION | sed 's/ /\n/g' | tail -n2 | head -n1 | sed 's/\./\n/g' | tail -n1 | sed 's/.*0$/0/g;s/.*1$/1/g;s/.*2$/2/g;s/.*3$/3/g;s/.*4$/4/g;s/.*5$/5/g;s/.*6$/6/g;s/.*7$/7/g;s/.*8$/8/g;s/.*9$/9/g')"
