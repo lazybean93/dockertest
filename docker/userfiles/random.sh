242be73af3984b818bc784f02ee75e58322edf82
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-09-18 13:54:36 +0200

    Update Random.sh

--- docker/userfiles/random.sh
+++ docker/userfiles/random.sh
@@ -1,3 +1,3 @@
 MAX=65535
 RANDOM=$(printf "%d\n" 0x$(head --bytes 2 /dev/random | xxd | sed 's/  /\n/g' | head -n1 | sed 's/ //g;s/:/\n/g' | tail -n1))
-echo $(($(echo "0""$(echo "scale=3; $RANDOM"/"$MAX" | bc -l)" | sed 's/^00/0/g;s/^01/1/g')*10))
+echo $(echo "0""$(echo "scale=3; $RANDOM"/"$MAX" | bc -l)" | sed 's/^00/0/g;s/^01/1/g')*10 | bc
