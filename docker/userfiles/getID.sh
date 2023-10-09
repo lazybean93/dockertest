77579d9c340953487bd721408b5ac4362a2279c1
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-10-09 09:13:56 +0200

    Issue get ID: Update

--- docker/userfiles/getID.sh
+++ docker/userfiles/getID.sh
@@ -1,5 +1,6 @@
 for i in $(seq 0 127); do
-	ping -c 2 172.17.0.$i;
+	ping -c 1 172.17.0.$i
+	echo ----------------
 done > ping.txt
 cat ping.txt | grep -v '100% packet loss' | grep -C1 '% packet loss' | grep ping | sed 's/ /\n/g' | grep [0-9] > ip.txt
 #rm ping.txt
