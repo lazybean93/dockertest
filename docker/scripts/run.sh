8f1915553edc3ce284d92d0a3a2cd202c6b5ca59
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-10-25 14:22:44 +0200

    Issue get ID: Update

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -3,7 +3,7 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname $SCRIPT`
 
-for i in $(seq 1 10); do
+for i in $(seq 0 10); do
 	#--name = generalworker_$i
 	#--rm = automatically remove container when exists
 	# -i  = keep stdin open
@@ -11,7 +11,7 @@ for i in $(seq 1 10); do
 	# -d  = detach
 	# -p  = portmapping
 	# dockertest:latest = name
-	docker run --name generalworker_$i --rm -i -t -d -p $((2200+$i)):22 -p $((7000+$i)):5901/tcp dockertest:latest &
+	docker run --name generalworker_$(printf %02d $i) --rm -i -t -d -p $((2200+$i)):22 -p $((7000+$i)):5901/tcp dockertest:latest &
 	sleep 0.1
 done
 #while [ $(echo $(sh "$SCRIPTPATH"/../userfiles/load.sh 1)*100 | bc -l | sed 's/\./\n/g' | head -n1) -lt 10 ]; do
