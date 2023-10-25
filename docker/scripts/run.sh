8ee5dfa23a13592148f028088b7711d63b7bb8a2
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-10-25 14:54:37 +0200

    Issue get ID: Update

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -3,23 +3,21 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname $SCRIPT`
 MAX_I=9
+NAME="generalworker_"
 FILE="/home/automate/id.txt"
+
 for i in $(seq 0 $MAX_I); do
-	#--name = generalworker_$i
+	#--name = "$NAME""$i"
 	#--rm = automatically remove container when exists
 	# -i  = keep stdin open
 	# -t  = allocate pseudo tty
 	# -d  = detach
 	# -p  = portmapping
 	# dockertest:latest = name
-	docker run --name generalworker_$(printf %03d $i) --rm -i -t -d -p $((2200+$i)):22 -p $((7000+$i)):5901/tcp dockertest:latest &
+	docker run --name "$NAME""$(printf %03d $i)" --rm -i -t -d -p $((2200+$i)):22 -p $((7000+$i)):5901/tcp dockertest:latest &
 	sleep 0.1
 done
 
 for i in $(seq $MAX_I); do
-	docker exec generalworker_$(printf %03d $i) sh -c "echo $i > $FILE; echo $MAX_I >> $FILE;"
+	docker exec "$NAME""$(printf %03d $i)" sh -c "echo $i > $FILE; echo $MAX_I >> $FILE;"
 done
-#while [ $(echo $(sh "$SCRIPTPATH"/../userfiles/load.sh 1)*100 | bc -l | sed 's/\./\n/g' | head -n1) -lt 10 ]; do
-#	date;
-#	sleep 1
-#done
