32475cfc876ad18a27d632c2120f63095deeffa1
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-10-25 19:49:11 +0200

    Issue let Pancake run

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -13,27 +13,36 @@ echo "----"
 pwd
 echo "----"
 
-for i in $(seq 0 $MAX_I); do
-	# -d  = detach
-	# -i  = keep stdin open
-	# -t  = allocate pseudo tty
-	#--name = "$NAME""$i"
-	#--rm = automatically remove container when exists
-	# -p  = portmapping
-	# dockertest:latest = name
-	docker run -d \
-		-i \
-		-t \
-		--name "$NAME""$(printf %03d $i)" \
-		--mount type=bind,source="$(pwd)"/Downloads,target=/home/automate/Downloads \
-		--rm \
-		-d \
-		-p $((2200+$i)):22 \
-		-p $((15900+$i)):5901 \
-		dockertest:latest &
-	sleep 0.1
-done
+#for i in $(seq 0 $MAX_I); do
+        # -d  = detach
+        # -i  = keep stdin open
+        # -t  = allocate pseudo tty
+        #--name = "$NAME""$i"
+        #--rm = automatically remove container when exists
+        # -p  = portmapping
+        # dockertest:latest = name
+#       docker run -d \
+#               -i \
+#               -t \
+#               --name "$NAME""$(printf %03d $i)" \
+#               --mount type=bind,source="$(pwd)"/Downloads,target=/home/automate/Downloads \
+#               --rm \
+#               -d \
+#               -p $((2200+$i)):22 \
+#               -p $((15900+$i)):5901 \
+#               dockertest:latest &
+#       sleep 0.1
+#done
 
-for i in $(seq 0 $MAX_I); do
-	docker exec "$NAME""$(printf %03d $i)" sh -c "echo $i > $FILE; echo $MAX_I >> $FILE;"
-done
+#for i in $(seq 0 $MAX_I); do
+#       docker exec "$NAME""$(printf %03d $i)" sh -c "echo $i > $FILE; echo $MAX_I >> $FILE;"
+#done
+
+docker run -d \
+	-i \
+	-t \
+	--name "Pancake" \
+	--rm \
+	-p 50022:22 \
+	-p 55901:5901 \
+	dockertest:latest &
