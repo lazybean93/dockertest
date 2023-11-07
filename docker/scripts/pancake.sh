304ec703bdaa48a2285c15dc4e6f90ecd23db15a
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-11-07 18:45:49 +0100

    Issue let Pancake run

--- docker/scripts/pancake.sh
+++ docker/scripts/pancake.sh
@@ -1,3 +1,10 @@
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+sleep 60
+cd "$SCRIPTPATH";
 docker run -d \
         -i \
         -t \
