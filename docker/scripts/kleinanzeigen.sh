4c7d71ef6c62b1933f768eac6fa1d6b48658159d
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-01-18 19:35:28 +0100

    restore originals

--- docker/scripts/kleinanzeigen.sh
+++ docker/scripts/kleinanzeigen.sh
@@ -25,6 +25,7 @@ FILE_Credentials="/home/automate/Kleinanzeigen/credentials.txt"
 ECHO_Mail="echo oliver.tiebe@gmail.com >> ""$FILE_Credentials"
 ECHO_Pass="echo '"';Ug$>bCfwM/\)"5b'"' >> ""$FILE_Credentials"
 SUFFIX="sh /home/automate/Kleinanzeigen/insertLatest.sh"
+SUFFIX="echo bla"
 CMD="$PREFIX""; ""$ECHO_Mail""; ""$ECHO_Pass""; ""$SUFFIX"
 
 docker exec --user automate "Kleinanzeigen" sh -c "$(echo $CMD)"
