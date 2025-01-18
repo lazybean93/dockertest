eeec00904d81fcb0dbe0661525162108c0972f84
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-01-18 14:49:01 +0100

    Update Keyboard

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -1,1 +1,7 @@
-docker build --pull --rm -f "Dockerfile" -t dockertest:latest "."
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd "$SCRIPTPATH";
+docker build --pull --rm -f "../Dockerfile" -t dockertest:latest ".."
