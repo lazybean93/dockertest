4859cd85d0a5eeee5c22ae5d97e981639bc450e5
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-05-14 07:38:44 +0200

    modified:   .gitignore modified:   docker/scripts/kleinanzeigen.sh modified:   docker/scripts/kleinanzeigen/docker-compose.yml renamed:    docker/scripts/pancake/docker-compose.yml -> docker/scripts/pancake/_docker-compose.yml renamed:    docker/scripts/pankeep/docker-compose.yml -> docker/scripts/pankeep/_docker-compose.yml

--- docker/scripts/kleinanzeigen.sh
+++ docker/scripts/kleinanzeigen.sh
@@ -3,29 +3,36 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-#sleep 60
-cd "$SCRIPTPATH";
-docker run -d \
-        -i \
-        -t \
-        --name "Kleinanzeigen" \
-        --rm \
-        -p 50222:22 \
-        -p 56101:5901 \
-        dockertest:latest &
-while [ -z "$(docker ps | grep Kleinanzeigen)" ]; do
-        sleep 1
-done
+if [ "$#" -ne 0 ]; then
+        sleep 60
+fi
 
-sleep 2
+if [ "$(docker ps | grep Kleinanzeigen | wc -l)" -lt 1]; then
+        cd "$SCRIPTPATH";
+        docker run -d \
+                -i \
+                -t \
+                --name "Kleinanzeigen" \
+                --rm \
+                -p 50222:22 \
+                -p 56101:5901 \
+                dockertest:latest &
+        while [ -z "$(docker ps | grep Kleinanzeigen)" ]; do
+                sleep 1
+        done
 
-PREFIX="export DISPLAY=:1 \
-    && xmodmap /home/automate/Xmodmap"
-FILE_Credentials="/home/automate/Kleinanzeigen/credentials.txt"
-ECHO_Mail="echo oliver.tiebe@gmail.com >> ""$FILE_Credentials"
-ECHO_Pass="echo '"';Ug$>bCfwM/\)"5b'"' >> ""$FILE_Credentials"
-SUFFIX="sh /home/automate/Kleinanzeigen/insertLatest.sh"
-SUFFIX="echo bla"
-CMD="$PREFIX""; ""$ECHO_Mail""; ""$ECHO_Pass""; ""$SUFFIX"
+        sleep 2
 
-docker exec --user automate "Kleinanzeigen" sh -c "$(echo $CMD)"
+        PREFIX="export DISPLAY=:1 \
+            ; xmodmap /home/automate/Xmodmap \
+            ; mkdir /home/automate/credentials"
+        FILE_Credentials="/home/automate/Kleinanzeigen/credentials.txt"
+        FILE_Credentials="/home/automate/credentials/kleinanzeigen.txt"
+        ECHO_Mail="echo oliver.tiebe@gmail.com >> ""$FILE_Credentials"
+        ECHO_Pass="echo '"';Ug$>bCfwM/\)"5b'"' >> ""$FILE_Credentials"
+        SUFFIX="sh /home/automate/Kleinanzeigen/insertLatest.sh"
+        SUFFIX="echo bla"
+        CMD="$PREFIX""; ""$ECHO_Mail""; ""$ECHO_Pass""; ""$SUFFIX"
+
+        docker exec --user automate "Kleinanzeigen" sh -c "$(echo $CMD)"
+fi
